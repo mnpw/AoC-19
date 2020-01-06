670032bafce9807d84495b1cc1d9f872f6d3725a
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2020-01-06 20:59:06 +0530

    Implement input parsing in Go for day 5

--- day-5/sol-a.go
+++ day-5/sol-a.go
@@ -4,11 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"reflect"
 	"strings"
 )
 
 var inputFile = flag.String("inputFile", "./input", "Input for solver")
 var debug = flag.Bool("debug", false, "Set true for debug output")
+
+func intcodeProcess(contents []string) int {
+	for i, val := range contents {
+		// opcode := val[len(val)-1]
+		fmt.Print(reflect.TypeOf(val), ", ", i, ", ", val, " ", len(val), " ", []byte(val), "\n")
+	}
+	return 1
+}
 
 func main() {
 	flag.Parse()
@@ -16,9 +25,11 @@ func main() {
 	contents := strings.Split(string(f), ",")
 
 	if *debug {
+		fmt.Print("Type of contents is ", reflect.TypeOf(contents))
 		fmt.Println("The len of contents if", len(contents), contents[0])
 		for index := 0; index < len(contents); index++ {
-			fmt.Print(contents[index])
+			fmt.Print(contents[index], ", ")
 		}
 	}
+	intcodeProcess(contents)
 }
